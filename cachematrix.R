ff4c408c209ee436f763c906ad191d92bda8f6c1
jkalloor <jkalloor@yahoo.com>
2015-03-20 15:25:38 -0500

    updated with caching R code fro matrix inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function "makeCacheMatrix" creates   an inverse of matrix in cache,
+# as way to cache potentially time consuming computations.  This take
+# advantage of the scoping rules of R language and how they can 
+# be manipulated to preserve state inside of an R object.
+
+# set function set the matrix
+# get function get the input matrix 
+# setmatrix compute the inverse of matrix and using "solve" and 
+# getmatrix get the inverse matrix form cache   
+# cachesSolve  check if inverse  exits in cache. If it is not NULL it use
+# it otherwise compute the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        # im will hold the inverse matrix initially set NULL
+        
+        im<- NULL  
+        # A set function, set a matrix to object 
+        # note:`<<-` to assign a value to an objectin an environment 
+        # different from the current environment.
+        
+        set<-function(y){
+                x<<-y
+                im<<-NULL
+        }
+        
+        # get function get the input matrix 
+        
+        get<-function()x
+        
+        #  set the inversed matrix 
+        
+        setmatrix<-function(solve)im<<- solve
+        
+        getmatrix<-function()im
+        # Preserve the list of the used functions.
+        list(set=set, get=get,
+             setmatrix=setmatrix,
+             getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+        im<-x$getmatrix()
+        if(!is.null(im)){
+                message("getting cached data")
+                return(im)
+        }
+        matrix<-x$get()
+        im<-solve(matrix)
+        x$setmatrix(im)
+        im
 }
+
